b99080a252ff4be932227c6a6be235a56125a3a7
DeeAnnEye <danielashulamithjustin@gmail.com>
2021-04-18 06:41:00 +0530

    Added crypto.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var crypto = require('../utils/crypto')
 var router = express.Router();
+// const axios = require('axios'); 
 
  // GET: all users
- router.get('/', function(req, res, next) {        
-  res.json({msg : 'hello'});  
+ router.get('/', function(req, res, next) {    
+    
 })
 
 
