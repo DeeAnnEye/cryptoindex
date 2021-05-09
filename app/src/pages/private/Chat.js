a1f9f05727954bd54d8575249ee413325783abb5
DeeAnnEye <danielashulamithjustin@gmail.com>
2021-05-09 17:44:49 +0530

    Added font-awesome

--- app/src/pages/private/Chat.js
+++ app/src/pages/private/Chat.js
@@ -4,13 +4,13 @@ const Chat = () => {
     return (
         <div className="chat-container">
       <header className="chat-header">
-        <h1><i className="fas fa-smile"></i> CryptoChat</h1>
+        <h1>CryptoChat</h1>
         <a id="leave-btn" className="btn">Leave Room</a>
       </header>
       <main className="chat-main">
         <div className="chat-sidebar">
-          <h3><i className="fas fa-comments"></i> Room Name:</h3>
-          <h2 id="room-name"></h2>
+          {/* <h3><i className="fas fa-comments"></i> Room Name:</h3>
+          <h2 id="room-name"></h2> */}
           <h3><i className="fas fa-users"></i> Users</h3>
           <ul id="users"></ul>
         </div>
@@ -25,7 +25,7 @@ const Chat = () => {
             required
             autoComplete="off"
           />
-          <button className="btn"><i className="fas fa-paper-plane"></i> Send</button>
+          <button className="btn"><i className="fas fa-paper-plane"></i></button>
         </form>
       </div>
     </div>
